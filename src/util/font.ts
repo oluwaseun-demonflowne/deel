99245934e86478ed93569e41099e9e861e678a30
demonflowne <ibuemmanuel60@gmail.com>
2025-01-10 16:00:55 +0100

    removed unused var

--- src/util/font.ts
+++ src/util/font.ts
@@ -1,4 +1,3 @@
-import { Bodoni_Moda, Oxanium } from "next/font/google";
+import { Oxanium } from "next/font/google";
 
 export const inter = Oxanium({ subsets: ["latin"] });
-export const Bodoni = Bodoni_Moda({ subsets: ["latin"] });
