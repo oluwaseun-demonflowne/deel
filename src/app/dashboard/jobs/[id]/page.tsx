0255e502b9a46fa6e828d2b797a770594533181b
demonflowne <ibuemmanuel60@gmail.com>
2025-01-18 17:06:25 +0100

    made text bigger

--- src/app/dashboard/jobs/[id]/page.tsx
+++ src/app/dashboard/jobs/[id]/page.tsx
@@ -21,7 +21,7 @@ const Page = () => {
 
   return (
     <div className="px-3 md:px-6 flex gap-6 md:gap-0 max-md:flex-col border py-6">
-      <div className="flex md:w-[70%] border-r md:pr-6 flex-col gap-4">
+      <div className="flex md:w-[70%] md:border-r md:pr-6 flex-col gap-4">
         <div className="flex items-center space-x-2">
           <Image
             src={"/wellspring.jfif"}
@@ -33,7 +33,7 @@ const Page = () => {
           />
           <div className="flex flex-col justify-center">
             <h1 className="text-xl md:text-3xl  font-semibold">{job[0].jobTitle}</h1>
-            <div className="flex flex-wrap md:gap-4 items-center text-[15px]">
+            <div className="flex flex-wrap gap-1  md:gap-4 items-center text-[15px]">
               <p className="flex items-center">
                 <CiLocationOn className="text-xl" /> {job[0].jobLocation}
               </p>
@@ -41,10 +41,10 @@ const Page = () => {
             </div>
           </div>
         </div>
-        <h4 className="text-sm tracking-wide">{job[0].jobDescription}</h4>
+        <h4 className="text-[15px] tracking-wide">{job[0].jobDescription}</h4>
         <div>
           <p className="font-bold text-[#4098fd]">Skills to have</p>
-          <ul className="flex flex-wrap text-sm gap-2">
+          <ul className="flex flex-wrap text-[15px] gap-2">
             {job[0].jobSkills.map((i, index) => (
               <li className="" key={i}>
                 {i}
@@ -61,7 +61,7 @@ const Page = () => {
         </div>
         <div>
           <p className="font-bold text-[#4098fd]">Your job responsibilities</p>
-          <ul className="text-sm space-y-1 pl-4 mt-2">
+          <ul className="text-[15px] space-y-1 pl-4 mt-2">
             {job[0].jobResponsibilities.map((i) => (
               <li className="list-disc" key={i}>
                 {i}
@@ -73,15 +73,15 @@ const Page = () => {
       <div className="md:w-[30%] space-y-4 md:px-6">
         <div>
           <h4 className="font-bold">-Job Experience</h4>
-          <h4 className="text-sm tracking-wide">{job[0].jobExperience}</h4>
+          <h4 className="text-[15px] tracking-wide">{job[0].jobExperience}</h4>
         </div>
         <div>
           <h4 className="font-bold">-Job Education</h4>
-          <h4 className="text-sm tracking-wide">{job[0].jobEducation}</h4>
+          <h4 className="text-[15px] tracking-wide">{job[0].jobEducation}</h4>
         </div>
         <div>
           <h4 className="font-bold">-Job Compensation</h4>
-          <h4 className="text-sm tracking-wide">{job[0].jobCompensation}</h4>
+          <h4 className="text-[15px] tracking-wide">{job[0].jobCompensation}</h4>
         </div>
         <button className="bg-[#4098fd] flex gap-1 items-center justify-center font-medium text-white w-44 h-12">
           Apply Now
